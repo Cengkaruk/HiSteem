2b279b62feacf842eda70d34fb0603f7ed65e5ac
Aji Kisworo Mukti <adzy.maniac@gmail.com>
2018-04-04 23:06:26 +0700

    Use the empty string '' to start the list

--- App/Sagas/TagSagas.js
+++ App/Sagas/TagSagas.js
@@ -9,11 +9,13 @@ export function * getTag () {
 
   if (currentTags.length <= 0) {
     try {
-      currentTags = yield call(api.getTrendingTagsAsync, 'steem', 150)
+      currentTags = yield call(api.getTrendingTagsAsync, '', 150)
     } catch (error) {
       yield put(TagActions.tagFailure())
     }
   }
+
+  currentTags = currentTags.slice(1)
 
   yield put(TagActions.tagSuccess(currentTags))
 }
