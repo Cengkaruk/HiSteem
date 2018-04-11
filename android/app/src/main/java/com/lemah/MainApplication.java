6a745b62986db8693f3897aea64cd162bf13860a
Aji Kisworo Mukti <adzy.maniac@gmail.com>
2018-04-11 11:55:59 +0700

    Add react-native-image-picker

--- android/app/src/main/java/com/lemah/MainApplication.java
+++ android/app/src/main/java/com/lemah/MainApplication.java
@@ -3,6 +3,7 @@ package com.lemah;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n(),
             new ReactNativeConfigPackage()
